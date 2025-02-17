c2e3d83d6ffe032e49a8c09b8a970818bb2aa303
Alexander_Ovchar <azalexxx02@gmail.com>
2025-02-17 21:40:25 +0200

    Set up basic D3 chart structure with SVG and dimensions

--- 1/index.js
+++ 1/index.js
@@ -1,8 +1,23 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-async function loadData() {
+async function draw(){
   const data = await d3.json("data.json")
-  console.log(data)
+
+  const dimensions = {
+    width: 800,
+    height: 800,
+
+  }
+
+  const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height)
+
+  // const xScale = d3.scaleLinear()
+  //   .domain([0, d3.max(data, d => d.age)])
+  //   .range([0, dimensions.width])
+    
 }
 
-loadData()
+draw()
