80ebf33ca55a4c125cc6a0d3c5012b48ddcdce8e
Alexander_Ovchar <azalexxx02@gmail.com>
2025-02-17 21:17:45 +0200

    Add D3 data binding to list items

--- 1/index.js
+++ 1/index.js
@@ -1,5 +1,16 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-const pD3 = d3.select("body").append("p").text("hello d3!").attr("class", "d3-text");
+const data = [10, 20, 30, 40, 50];
 
-console.log(pD3)
+const el = d3.selectAll("li").data(data).join(
+  enter => {
+    return enter.append("li")
+    .style("color", "red")
+  },
+  update => {
+    return update.style("color", "blue")
+  },
+  exit => exit.remove()
+).text(d => d)
+
+console.log(el)
