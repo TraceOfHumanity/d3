687a77809e44bf9784dcff52a0c74ae9005a8ae2
Alexander_Ovchar <azalexxx02@gmail.com>
2025-02-17 21:21:59 +0200

    Refactor D3 data binding with explicit enter and exit methods

--- 1/index.js
+++ 1/index.js
@@ -2,15 +2,23 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const data = [10, 20, 30, 40, 50];
 
-const el = d3.selectAll("li").data(data).join(
-  enter => {
-    return enter.append("li")
-    .style("color", "red")
-  },
-  update => {
-    return update.style("color", "blue")
-  },
-  exit => exit.remove()
-).text(d => d)
+const el = d3.selectAll("li").data(data)
+  // .join(
+  //   enter => {
+  //     return enter.append("li")
+  //       .style("color", "red")
+  //   },
+  //   update => {
+  //     return update.style("color", "blue")
+  //   },
+  //   exit => exit.remove()
+  // )
+  .text(d => d)
+
+  el.enter()
+    .append("li")
+    .text(d => d)
+
+  el.exit().remove()
 
 console.log(el)
