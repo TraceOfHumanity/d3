a19a8709417a75c273fd9e0c7b10604613dd905e
Alexander_Ovchar <azalexxx02@gmail.com>
2025-02-17 22:07:41 +0200

    Add scales and render scatter plot with humidity and apparent temperature

--- 1/index.js
+++ 1/index.js
@@ -1,23 +1,52 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-async function draw(){
+async function draw() {
   const data = await d3.json("data.json")
+
+  const xAccessor = d => d.currently.humidity
+  const yAccessor = d => d.currently.apparentTemperature
 
   const dimensions = {
     width: 800,
     height: 800,
-
+    margin: {
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50
+    },
+    ctrWidth: 0,
+    ctrHeight: 0
   }
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+  dimensions.ctrHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+
 
   const svg = d3.select("#chart")
     .append("svg")
     .attr("width", dimensions.width)
     .attr("height", dimensions.height)
 
-  // const xScale = d3.scaleLinear()
-  //   .domain([0, d3.max(data, d => d.age)])
-  //   .range([0, dimensions.width])
-    
+  const ctr = svg.append("g")
+    .attr("transform", `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
+
+
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, xAccessor))
+    .range([0, dimensions.ctrWidth])
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(data, yAccessor))
+    .range([dimensions.ctrHeight, 0])
+
+  ctr.selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", d => xScale(xAccessor(d)))
+    .attr("cy", d => yScale(yAccessor(d)))
+    .attr("r", 5)
+    .attr("fill", "red")
 }
 
 draw()
