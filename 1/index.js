ecb4dfec8fd3bbf1cb09ab7e1682179197acd0c0
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-06 23:00:12 +0300

    Enhance D3 chart: update data handling, implement dynamic circle rendering, and refine dimensions calculation

--- 1/index.js
+++ 1/index.js
@@ -89,7 +89,10 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 async function draw() {
-  const data = await d3.json("data.json")
+  const dataset = await d3.json("data.json")
+
+  const xAccessor = d => d.currently.humidity
+  const yAccessor = d => d.currently.apparentTemperature
 
   let dimensions = {
     width: 800,
@@ -100,8 +103,12 @@ async function draw() {
       bottom: 50,
       left: 50
     },
-    
+    ctrWidth: 0,
+    ctrHeight: 0
   }
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+  dimensions.ctrHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
 
   const svg = d3.select("#chart")
   .append("svg")
@@ -111,8 +118,25 @@ async function draw() {
   const ctr = svg.append("g")
   .attr("transform", `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
 
-  ctr.append("circle")
-  .attr('r', 15)
+  const xScale = d3.scaleLinear()
+  .domain(d3.extent(dataset, xAccessor))
+  .rangeRound([0, dimensions.ctrWidth])
+  .clamp(true)
+
+  const yScale = d3.scaleLinear()
+  .domain(d3.extent(dataset, yAccessor))
+  .rangeRound([dimensions.ctrHeight, 0])
+  .clamp(true)
+
+  ctr.selectAll("circle")
+  .data(dataset)
+  .join("circle")
+  .attr("cx", d => xScale(xAccessor(d)))
+  .attr("cy", d => yScale(yAccessor(d)))
+  .attr("r", 5)
+  .attr("fill", "red")
+  
+  
   
 }
 
