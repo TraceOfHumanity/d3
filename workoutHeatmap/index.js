d8a6974c6fad4db15f8618c9e0c655de3a242bab
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-07 20:15:26 +0300

    Add workout data: include new entries for running, swimming, and crossfit workouts in data.json; enhance D3 chart to visualize workout durations with a color scale

--- workoutHeatmap/index.js
+++ workoutHeatmap/index.js
@@ -3,10 +3,8 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 async function draw() {
   const dataset = await d3.json("data.json")
 
-  //365 days / week
   const days = 365
   const weeks = Math.ceil(days / 7)
-  console.log(weeks) 
   
   const box = 30
   const width = box * weeks
@@ -17,6 +15,9 @@ async function draw() {
     height: height,
   };
 
+  const colorScale = d3.scaleLinear()
+  .domain(d3.extent(dataset.map(d => d["workout-duration"])))
+  .range(["#ffffff", "#ff0000"])
 
   const svg = d3.select("#heatmap")
     .append("svg")
@@ -24,6 +25,15 @@ async function draw() {
     .attr("height", dimensions.height)
     .style("background-color", "gray")
     
+  svg.append("g")
+    .selectAll("rect")
+    .data(dataset)
+    .join("rect")
+    .attr("width", box - 3)
+    .attr("height", box - 3)
+    .attr("x", (d, i) => box * ((i / 7) | 0))
+    .attr("y", (d, i) => box * ((i % 7)))
+    .attr("fill", d => colorScale(d["workout-duration"]))
 }
 
 draw()
