0db3b61688fdfe1a81354e6969d5f5433123a21f
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-08 19:51:42 +0300

    Refactor D3 histogram implementation: introduce dynamic metric selection for humidity, temperature, and other weather data, enhancing interactivity and visualization capabilities.

--- Histogram/app.js
+++ Histogram/app.js
@@ -2,9 +2,6 @@ async function draw() {
   // Data
   const dataset = await d3.json('data.json')
 
-  const xAccessor = (d) => d.currently.humidity
-  const yAccessor = (d) => d.length
-
   // Dimensions
   let dimensions = {
     width: 800,
@@ -29,50 +26,64 @@ async function draw() {
       `translate(${dimensions.margins}, ${dimensions.margins})`
     )
 
-  const xScale = d3.scaleLinear()
-    .domain(d3.extent(dataset, xAccessor))
-    .range([0, dimensions.ctrWidth])
-    .nice()
-
-  const bins = d3.bin()
-    .domain(xScale.domain())
-    .value(xAccessor)
-    // .thresholds(d3.range(0, 1, 0.1))
-    .thresholds(10)
-
-  const binGroups = bins(dataset)
-  const padding = 1
-
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(binGroups, yAccessor)])
-    .range([dimensions.ctrHeight, 0])
-    .nice()
-
-  ctr.selectAll("rect")
-    .data(binGroups)
-    .join("rect")
-    .attr("width", d => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding]))
-    .attr("height", d => dimensions.ctrHeight - yScale(yAccessor(d)))
-    .attr("x", d => xScale(d.x0))
-    .attr("y", d => yScale(yAccessor(d)))
-    .attr("fill", "#01c5c1")
-
-  ctr.append("g")
+  const labelGroup = ctr.append("g")
     .classed("bar-labels", true)
-    .selectAll("text")
-    .data(binGroups)
-    .join("text")
-    .text(d => yAccessor(d))
-    .attr("x", d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
-    .attr("y", d => yScale(yAccessor(d)))
-    .attr("text-anchor", "middle")
-
-  const xAxis = d3.axisBottom(xScale)
 
   const xAxisGroup = ctr.append("g")
     .style("transform", `translateY(${dimensions.ctrHeight}px)`)
-    .call(xAxis)
 
+  function histogram(metric) {
+    const xAccessor = (d) => d.currently[metric]
+    const yAccessor = (d) => d.length
+
+
+    const xScale = d3.scaleLinear()
+      .domain(d3.extent(dataset, xAccessor))
+      .range([0, dimensions.ctrWidth])
+      .nice()
+
+    const bins = d3.bin()
+      .domain(xScale.domain())
+      .value(xAccessor)
+      .thresholds(10)
+
+    const binGroups = bins(dataset)
+    const padding = 1
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(binGroups, yAccessor)])
+      .range([dimensions.ctrHeight, 0])
+      .nice()
+
+    ctr.selectAll("rect")
+      .data(binGroups)
+      .join("rect")
+      .attr("width", d => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding]))
+      .attr("height", d => dimensions.ctrHeight - yScale(yAccessor(d)))
+      .attr("x", d => xScale(d.x0))
+      .attr("y", d => yScale(yAccessor(d)))
+      .attr("fill", "#01c5c1")
+
+    labelGroup.selectAll("text")
+      .data(binGroups)
+      .join("text")
+      .text(d => yAccessor(d))
+      .attr("x", d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+      .attr("y", d => yScale(yAccessor(d)))
+      .attr("text-anchor", "middle")
+
+    const xAxis = d3.axisBottom(xScale)
+
+    xAxisGroup.call(xAxis)
+  }
+
+  d3.select("#metric")
+    .on("change", function (e) {
+      e.preventDefault()
+      histogram(this.value)
+    })
+
+  histogram("humidity")
 }
 
 draw()
